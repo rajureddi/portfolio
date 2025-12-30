8627143fcc2be2a1339a2c2fee4faecfc771ebf6
RAJUReDDY <69119292+rajureddi@users.noreply.github.com>
2025-12-30 23:16:21 +0530

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="raju2525"
+        username="rajureddi"
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
