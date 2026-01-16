8e80b2a6d6c4e5aeea587e336517d99fbd93188b
rajureddi <rajubandam694@gmail.com>
2026-01-17 00:21:47 +0530

    message

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,7 +3,6 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-<<<<<<< HEAD
 function ProjectCard(props) {
   return (
     <Card className="project-card-view h-100">
@@ -45,45 +44,9 @@ function ProjectCard(props) {
             </Button>
           )}
         </div>
-=======
-
-function ProjectCards(props) {
-  return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
       </Card.Body>
     </Card>
   );
 }
-<<<<<<< HEAD
 
 export default ProjectCard;
-=======
-export default ProjectCards;
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-<<<<<<< HEAD
 
 import leaf from "../../Assets/Projects/leaf.png";
 import backlogWallah from "../../Assets/Projects/backlog-wallah.jpg";
@@ -13,17 +12,6 @@ import suicide from "../../Assets/Projects/suicide.png";
 
 import traffic from "../../Assets/Projects/smart-traffic.png";
 import turantpay from "../../Assets/Projects/turantpay.png";
-=======
-import leaf from "../../Assets/Projects/leaf.png";
-import bwImage from '../../Assets/Projects/bw.jpg';
-import sign from "../../Assets/Projects/voice2sign.png";
-
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
 
 function Projects() {
   return (
@@ -31,16 +19,11 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-<<<<<<< HEAD
           My Recent <strong className="purple">Works</strong>
-=======
-          My Recent <strong className="purple">Works </strong>
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-<<<<<<< HEAD
 
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           
@@ -95,36 +78,10 @@ function Projects() {
          
 
           {/* NER */}
-=======
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bwImage}
-              isBlog={false}
-              title="BACKLOG WALLAH!"
-              description="Python based Android App build with kivyMD module which is a learning application helps in clearing backlogs"
-              ghLink="https://github.com/rajureddi/BACKLOG-WALLAH-"
-              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={sign}
-              isBlog={false}
-              title="voice 2 sign"
-              description="voice2sign is a sign language to voice and voice to sign language conversion application with the help of machine learning and deep learning."
-              ghLink="https://github.com/rajureddi/voice2sign"
-              
-            />
-          </Col>
-
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-<<<<<<< HEAD
               title="Named Entity Recognition (BERT)"
               description="A BERT-based Named Entity Recognition system trained on the CoNLL-2003 dataset to extract structured information such as person names, organizations, locations, dates, and numerical entities from unstructured text."
               ghLink="https://github.com/rajureddi/ner-bert"
@@ -134,21 +91,11 @@ function Projects() {
 
 
           {/* PLANT AI */}
-=======
-              title="NAMED ENTITY RECOGNITION-BERT"
-              description="Named Entity Recognition (NER) is a subtask of information extraction that locates and classifies named entities mentioned in unstructured text into pre-defined categories such as the person names, organizations, locations, medical codes, time expressions, quantities, monetary values, percentages, etc. trained with conell2003 dataset."
-              ghLink="https://github.com/rajureddi/ner-bert"
-                           
-            />
-          </Col>
-
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
               title="Plant AI"
-<<<<<<< HEAD
               description="A deep learning-based plant disease detection system trained on Kaggle datasets using CNNs and transfer learning (ResNet34). Achieved up to 98% accuracy across 38 plant disease classes."
               ghLink="https://github.com/rajureddi/PlantAI"
               tags={["Deep Learning", "Computer Vision", "Agriculture"]}
@@ -157,19 +104,10 @@ function Projects() {
 
 
           {/* SENTIMENT */}
-=======
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/rajureddi/PlantAI"
-              
-            />
-          </Col>
-
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-<<<<<<< HEAD
               title="Sentiment Analysis Based Ad Generation"
               description="An NLP-based system that analyzes user sentiment from feedback and automatically generates targeted advertisements to improve product reach and market growth."
               ghLink="https://github.com/rajureddi/sentiment_analysis"
@@ -178,20 +116,10 @@ function Projects() {
           </Col>
 
           {/* FACE + EMOTION */}
-=======
-              title="SENTIMENT ANALYSIS BASED AD GENERATION"
-              description="NATURAL LANGUAGE PROCESSING based project analysis the feedback from the users based on their sentiment it generates ad for market growth"
-              ghLink="https://github.com/rajureddi/sentiment_analysis"
-
-            />
-          </Col>
-
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-<<<<<<< HEAD
               title="Face Recognition & Emotion Detection"
               description="A CNN-based emotion recognition system trained on the FER-2013 dataset, combined with OpenCV for face detection to predict human emotions from images and live video feeds."
               ghLink="https://github.com/rajureddi/Face-Recognition-and-Emotion-Detection"
@@ -199,15 +127,6 @@ function Projects() {
            />
           </Col>
 
-=======
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/rajureddi/Face-Recognition-and-Emotion-Detection"
-              
-            />
-          </Col>
->>>>>>> d1f6ee981cb0b8c4fe648fa87b1c1b4a63f6b5d0
         </Row>
       </Container>
     </Container>
